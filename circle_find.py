1938e8a73cc6a35a192b45a85124cd1c55a36f74
not0ptimist <optiboxfortrash@yandex.ru>
2022-04-09 01:16:50 +0500

    Игра работает, при праавильном вводе значений

---
+++ circle_find.py
@@ -0,0 +1,29 @@
+def circle_find(t, con_bomb, razmer):
+    sdvigi = [1, razmer - 1, razmer, razmer + 1]# Centr
+    left_side = [t - razmer, t - razmer + 1, t + 1, t + razmer, t + razmer + 1]# Left if == 0
+    right_side = [t - razmer - 1, t - razmer, t - 1, t + razmer - 1, t + razmer]# Right if == razmer - 1:
+    # print(left_side, right_side, len(con_bomb), t % razmer)
+    sum_bomb = 0
+    if t % razmer == 0:
+        for i in left_side:
+            if i >= 0:
+                sum_bomb += con_bomb[i]
+    elif t % razmer == razmer - 1:
+        for i in right_side:
+            if i < len(con_bomb):
+                sum_bomb += con_bomb[i]
+    else:
+        for i in sdvigi:
+            if t + i < len(con_bomb):
+                sum_bomb += con_bomb[t + i]
+            if t - i >= 0:
+                sum_bomb += con_bomb[t - i]
+    # print(sum_bomb)
+    return sum_bomb
+
+
+# circle_find(4, [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0], 4)
+# [0 0 1 0]
+# [0 0 0 0]
+# [1 0 0 1]
+# [0 0 1 0]
--- game.py
+++ game.py
@@ -3,75 +3,75 @@
 Вводим координаты, и смотрим результат.
 """
 import numpy as np
+from circle_find import circle_find
 
 
-def construct_bombs(bomb):#конструктор поля с минами
-    all_field = bomb ** 2
+def construct_bombs(bomb):# Конструктор поля с минами
+    all_field = bomb ** 2# Длина поля
     zeros = np.zeros((all_field), dtype=np.int32)
-    zeros[:bomb] = 1
+    zeros[:bomb] = 1# Заполняем первую "строчку", мнимого квадратного поля
     np.random.shuffle(zeros)
-    zeros = zeros.reshape((bomb, bomb))
+    print(zeros)
     return zeros
 
 
-def construct_field(razmer):#конструктор поля с именами осей, для отображения
-    """Переделать на обычные вложенные списки, через insert добавить буквы
-    отображение сделать можно через цельную строку.
-    Проблема Numpy, это то что он с цифрами заточен работать, а не с буквами.
-    """
-    razmer_plus = razmer + 1
-    razmer_field = (razmer_plus) ** 2
-    zeros = np.zeros((razmer_field), dtype=np.int32)
-    zeros.resize((razmer_plus, razmer_plus))
-    dt = np.dtype([('name', np.unicode_, 2)])
-    zeros_str = np.array(zeros, dtype=dt)
-    for i in range(razmer_plus):
-        alpha = [chr(alpha) for alpha in range(65, 89)]
-        if i == 0:
-            zeros_str[0] = [x for x in range(razmer_plus)]
-        else:
-            zeros_str[i][0] = alpha[i - 1]
-    return zeros_str
+def construct_field(razmer):# Конструктор поля отображения игроку
+    alpha = [chr(alpha) for alpha in range(65, 89)]# Список алфавита
+    spisok = ['0' for x in range(razmer)]# Основное поле
+    matrix = [list(alpha[x]) for x in range(razmer)]# Формирование столбца букв, стобец "x"
+    for i in range(razmer):
+        matrix[i].extend(spisok)# Добавление полей к столбцу
+    matrix.insert(0, [str(x) for x in range(razmer + 1)])# Добавление верхней строки цифр, строка "y"
+    return matrix
 
 
-def proverka(x, y, con_bomb, con_field):#проверка координат на мину
-    """Пере"""
-    i = 0
-    # for stroka in con_field:
-        # print(np.where((x)))
-        # print(stroka)
-        # if stroka.where(x):
-        #     print(stroka)
-    # x = con_field.where(x) 
-    print(x, y, '!!!')
-    # y = y - 1
-    # if con_bomb[x][y] == '1':
-    #     print('popal')
-    # else:
-    #     print('live')
-    pass
+def proverka(x, y, con_bomb):# Проверка координат на мину
+    """Проверка на бомбу"""
+    tochka = 4 * (x - 65) + (int(y) - 1)
+    if con_bomb[tochka] == 1:
+        return False
+    else:
+        return True
 
 
-def proga():#набросок
+def podskazka(x, y, con_bomb, con_field):# Вывод кол-ва мин
+    razmer = len(con_field) - 1
+    tochka = 4 * (x - 65) + (int(y) - 1)
+    x = x - 64
+    y = int(y)
+    num_bomb = circle_find(tochka, con_bomb, razmer)# Подсчет ближайших мин
+    con_field[x][y] = str(num_bomb)# Вписываем в указанную клетку число мин
+    return con_field
+
+
+def proga():# Основной код игры
     osnov_arg = input('Введите число для расчета площади и кол-ва мин: ')
-    if osnov_arg.isnumeric():#проверка
-        osnov_arg = int(osnov_arg)#сразу меняем на число, т.к. потом только числом нужен данный аргумент
+    if osnov_arg.isnumeric():# Проверка на число, нужно расширять
+        osnov_arg = int(osnov_arg)# Сразу меняем на число, т.к. потом только числом нужен данный аргумент
         con_bomb = construct_bombs(osnov_arg)
         con_field = construct_field(osnov_arg)
-        print('cool')
-        while True:#слушаем на получение координат
-            print(con_field)
+        print('Приступаем!!!')
+        while True:# Слушаем на получение координат
+            for i in con_field:# Вывод текущего поля
+                for j in i:
+                    print(j, end=' ')
+                print('')
             vvod = input('Введите координаты, формата "строка.столбец"(B.3): ')
             if vvod == 'exit':
                 return print('Ну и уходи.')
-            x, y = vvod.split('.')
-            proverka(x, y, con_bomb, con_field)
-
+            elif '.' in vvod:
+                x, point, y = vvod[0], vvod[1], vvod[2:]# Через срез, для проверки на второй символ точку
+                x = ord(x.upper())# Преобразования буквы в номер ASCII
+                if x >= 65 and x < 65 + osnov_arg and point == '.':
+                    if proverka(x, y, con_bomb) == False:
+                        return print('BOOM!!!')
+                    else:
+                        podskazka(x, y, con_bomb, con_field)
+            else:
+                print('Форма записи не верна, повторите попытку')
     else:
-        print('foooo')
+        print('Фуууу, слабак')
 
-# construct_field(osnov_arg)
-# construct_field(7)
 
 if __name__ == '__main__':
     proga()
